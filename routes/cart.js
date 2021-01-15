6f348931fb7466c4138e25160a1eab46a40fdbf4
ivanbeljan <ivanbeljanofficial@gmail.com>
2021-01-15 09:12:40 +0100

    fixed bugs in cart.js routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 require('../middleware/index')();
 const cartController = require('../controllers/cart');
 
 
 
-router.get("/cart", isUser, userController.getCart)
-      .get("/checkout", isUser, userController.getCheckout);
+router.get("/cart", isUser, cartController.getCart)
+      .get("/checkout", isUser, cartController.getCheckout);
 
 
 router.post('/cart', isUser, cartController.postCart)
       .post('/cart-eBook', isUser, cartController.postCartEBook)
       .post('/CartRemove', isUser, cartController.postCartRemove);
+
+
+
+module.exports = router;
