7c89bfbf382fa81b9473d66d2a624b0c22e6971d
ivanbeljan <ivanbeljanofficial@gmail.com>
2021-03-29 11:02:40 +0200

    refactored users.js controller, dropped callbacks, now using promises

--- controllers/users.js
+++ controllers/users.js
@@ -2,123 +2,97 @@ const User = require("../models/user");
 const passport = require('passport');
 const nodemailer = require("nodemailer");
 const crypto = require('crypto');
-const async = require('async');
 require('../middleware/index')();
 
 
 exports.postSignUp = (req, res) => {
   req.body.username = req.body.username.toLowerCase();
   const newUser = new User({username: req.body.username, email: req.body.email, status: "member", cart: { items: [] }});
-  User.register(newUser, req.body.password, (err, user) => {
-      if(err){
-          return res.render("users/signup", { path: "users/signup"});
-      } else {
-              const smtpTransport = nodemailer.createTransport({
-                service: 'Gmail', 
-                auth: {
-                  user: process.env.GMAILUSER,
-                  pass: process.env.GMAILPW
-                }
-              });
-              const mailOptions = {
-                to: user.email,
-                from: process.env.GMAILUSER,
-                subject: 'Zahvaljujemo vam na registraciji!',
-                text: 'Šaljemo vam ovaj mail kao potvrdu vaše registracije\n\n' +
-                  'username:' + user.username + '\n\n' +
-                  'Ako se niste vi registrirali, molimo vas obrišite račun\n'
-              };
-              smtpTransport.sendMail(mailOptions, function(err) {
-                console.log('mail sent to' + user.email);
-              });
-            }
-          passport.authenticate("local")(req,res, () => {
-              res.redirect("/books");
-          })
-      
+  User.register(newUser, req.body.password).then(user => { 
+    const smtpTransport = nodemailer.createTransport({
+      service: 'Gmail', 
+      auth: {
+        user: process.env.GMAILUSER,
+        pass: process.env.GMAILPW
+      }
+    });
+    const mailOptions = {
+      to: user.email,
+      from: process.env.GMAILUSER,
+      subject: 'Zahvaljujemo vam na registraciji!',
+      text: 'Šaljemo vam ovaj mail kao potvrdu vaše registracije\n\n' +
+        'username:' + user.username + '\n\n' +
+        'Ako se niste vi registrirali, molimo vas obrišite račun\n'
+    };
+    smtpTransport.sendMail(mailOptions).then(() => { 
+        console.log('mail sent to' + user.email);
+    }).catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    })
+    passport.authenticate("local")(req,res, () => {
+        res.redirect("/books");
+    })
+  }).catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
   })
 };
 
 exports.postLogin = 
-  passport.authenticate("local", 
-{
+  passport.authenticate("local", {
     successRedirect: "/books",
     failureRedirect: "/login"
-}), () => {
+  }), () => {
 }
 
 
 exports.postAdminRegister = (req, res) => {
-
     if (req.body.code === process.env.ADMIN_CODE){
         let update = { status: "admin"};
-    User.findByIdAndUpdate(req.user._id, update, (err) => {
-        if(err) {
-            res.redirect("/");
-        } else {
+        User.findByIdAndUpdate(req.user._id, update).then(() => {
             res.render("users/settings", { path: 'users/settings'});
-        }
-    } )
-} else {
-    res.redirect("/");
-}
+        }).catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+    } else {
+        res.redirect("/");
+    }
 }
 
 
 exports.postSettings = (req, res, next) => {
-  const update = {email: req.body.email}
-  User.findByIdAndUpdate(req.user._id, update).then(result => {
-      res.render('index/landing', {path: '/'});
-      console.log(result);
-  }).catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-  })
+    const update = {email: req.body.email}
+    User.findByIdAndUpdate(req.user._id, update).then(result => {
+        res.render('index/landing', {path: '/'});
+        console.log(result);
+    }).catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    })
 }
 
 
-// Code from: http://sahatyalkabov.com/how-to-implement-password-reset-in-nodejs/
 exports.postReset = (req, res, next) => {
-    async.waterfall([
-        function(done) {
-          crypto.randomBytes(32, (err, buf) => {
-            let token = buf.toString('hex');
-            done(err, token);
-          });
-        },
-        function(token, done) {
-          /** Find User
-     * @param {Object} email: req.body.email - User's email
-     * @param {Function} 
-     */
-          User.findOne({ email: req.body.email }, (err, user) => {
-            if (!user) {
-              console.log('error' + 'No account with that email address exists.');
-              return res.redirect('/');
-            }
-    /**Creates token */
-            user.resetPasswordToken = token;
-    /** Token duration */
-            user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-    
-            user.save(function(err) {
-              done(err, token, user);
-            });
-          });
-        },
-    /** Function processing request
-     * @param {String} token- token given for reseting password
-     *  @param {User} user- user who made request
-     */
-        (token, user, done) => {
+    const token = crypto.randomBytes(32).toString('hex');
+    User.findOne({ email: req.body.email }).then(user => {
+          /**Creates token */
+          user.resetPasswordToken = token;
+          /** Token duration */
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+          user.save();
           let smtpTransport = nodemailer.createTransport({
             service: 'Gmail', 
             auth: {
               user: process.env.GMAILUSER,
               pass: process.env.GMAILPW
             }
-          });
+          })
           let mailOptions = {
             to: user.email,
             from: process.env.GMAILUSER,
@@ -128,52 +102,37 @@ exports.postReset = (req, res, next) => {
               'http://' + req.headers.host + '/reset/' + token + '\n\n' +
               'If you did not request this, please ignore this email and your password will remain unchanged.\n'
           };
-          smtpTransport.sendMail(mailOptions, (err) => {
-            console.log('mail sent');
-            req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-            done(err, 'done');
-          });
-        }
-      ], (err) => {
-        if (err) return next(err);
-        res.redirect('/');
-      });
-    };
-
-
-exports.postResetToken = (req, res) => {
-  async.waterfall([
-    function(done) {
-      /** Find User
- * @param {String} resetPasswordToken - Token for password reset.
- * @param {Callback} resetPasswordExpires - checks if token expired
- * @param {Function} 
- */
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-  /** if user not existing, send error message */
-        if (!user) {
-          req.flash('error', 'Password reset token is invalid or has expired.');
-          return res.redirect('back');
-        }
-        if(req.body.password === req.body.confirm) {
-          user.setPassword(req.body.password, function(err) {
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
-
-            user.save((err) => {
-              req.logIn(user, function(err) {
-                done(err, user);
-              });
-            });
+          smtpTransport.sendMail(mailOptions).then(() => {
+            console.log("reset email sent");
+            res.redirect('/login');
+          }).catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
           })
-        } else {
-            req.flash("error", "Passwords do not match.");
-            return res.redirect('back');
-        }
-      });
-    },
-    function(user, done) {
-/** mail protocol defined*/
+    }).catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+    })
+};
+
+
+exports.postResetToken = (req, res, next) => {
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now()}}).then(user => {
+      if(req.body.password === req.body.confirm) {
+        user.setPassword(req.body.password).then(() => {
+          user.resetPasswordToken = undefined;
+          user.resetPasswordExpires = undefined;
+          user.save();
+        }).catch(err => {
+          const error = new Error(err);
+          error.httpStatusCode = 500;
+          return next(error);
+        })
+      } else {
+        return res.redirect('back');
+      }
       var smtpTransport = nodemailer.createTransport({
         service: 'Gmail', 
         auth: {
@@ -188,25 +147,28 @@ exports.postResetToken = (req, res) => {
         text: 'Hello,\n\n' +
           'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
       };
-/** mail protocol */
-      smtpTransport.sendMail(mailOptions, function(err) {
-        req.flash('success', 'Success! Your password has been changed.');
-        done(err);
-      });
-    }
-  ], function(err) {
-    res.redirect('/');
-  });
+      smtpTransport.sendMail(mailOptions).then(() => {
+        console.log("password resetted successfully");
+        req.logIn(user, err => {
+          if (err) console.log(err);
+        });
+        res.redirect('/books');
+      }).catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+      })
+    })
 };
 
-exports.getResetToken = (req, res) => {
-  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-    if (!user) {
-      req.flash('error', 'Password reset token is invalid or has expired.');
-      return res.redirect('/');
-    }
-    res.render('users/resetpw', {token: req.params.token});
-  });
+exports.getResetToken = (req, res, next) => {
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now()}}).then(() => { 
+      res.render('users/resetpw', {token: req.params.token});
+    }).catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 }
 
 exports.postLogout = (req, res) => {
@@ -224,14 +186,14 @@ exports.getAdminRegister = (req, res) => {
 }
 
 
-exports.getSettings = (req,res) => {
-  User.findById(req.user._id, (err, foundUser) => {
-      if(err) {
-          return res.redirect("/");
-      } else {
-          res.render("users/settings", {path: 'users/settings', user: foundUser})
-      }
-  })
+exports.getSettings = (req,res, next) => {
+    User.findById(req.user._id).then(foundUser => {
+      res.render("users/settings", {path: 'users/settings', user: foundUser})
+    }).catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 }
 
 exports.getLogin = (req, res) => {
